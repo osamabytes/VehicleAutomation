ee6b818a26b71ed55964398ee047535ac6dbede1
osamabytes <osamahu96@gmail.com>
2024-04-30 13:07:17 +0500

    Inventory API in progress...

--- VehicleAutomation.InventoryAPI/Program.cs
+++ VehicleAutomation.InventoryAPI/Program.cs
@@ -1,11 +1,43 @@
+using MassTransit;
+using Serilog;
+using Serilog.Events;
+using VehicleAutomation.Data.DependencyInjection;
+using VehicleAutomation.Infrastructure.DependencyInjection;
+using VehicleAutomation.Mediator.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var connectionStr = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDatabaseService(connectionStr);
+builder.Services.AppServices();
+//configure serilog logger
+builder.Host.UseSerilog((ctx, lc) => lc
+    .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {CorrelationId} {Message:lj}{NewLine}{Exception}")
+    .WriteTo.File("logs/orderServiceLogs.txt", rollingInterval: RollingInterval.Day, outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {CorrelationId} {Message:lj}{NewLine}{Exception}")
+    .Enrich.FromLogContext()
+    .MinimumLevel
+    .Override("Microsoft", LogEventLevel.Information)
+);
+// configure masstransit rabbitmq
+var rabbitMQEnv = builder.Configuration.GetSection("RabbitMQ");
+builder.Services.AddMassTransit(m =>
+{
+    m.UsingRabbitMq((context, config) =>
+    {
+        config.Host($"{rabbitMQEnv["Host"]}/{rabbitMQEnv["Port"]}", rabbitMQEnv["VirtualHost"], host =>
+        {
+            host.Username(rabbitMQEnv["username"]);
+            host.Password(rabbitMQEnv["password"]);
+        });
+    });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// add mediator to service container
+builder.Services.AddMediatorInjection();
 
 var app = builder.Build();
 
